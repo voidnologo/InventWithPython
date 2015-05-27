802e9804d03b3c3cb487e752e0c8c565e9cd6e0d
voidnologo <void.no.logo@icloud.com>
2015-05-27 07:25:00 -0500

    Homework: ask name

---
+++ Homework/2015-01-28_ask_name.py
@@ -0,0 +1,16 @@
+print("What is your name? ")
+name = input()
+
+print("What is your age? ")
+age = input()
+
+print("What is your favorite color? ")
+color = input()
+
+print("What is your favorite food?")
+food = input()
+
+print("Hello, " + name +
+    ".  You are " + age +
+    " years old.  Your favorite color is " + color +
+    " and your favorite food is " + food + ".")
