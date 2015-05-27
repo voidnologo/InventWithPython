03167375cf7fe4317017e8aa47aac77c1c977ec4
voidnologo <void.no.logo@icloud.com>
2015-05-27 07:25:30 -0500

    Homework: numbers

---
+++ Homework/2015-01-28_numbers.py
@@ -0,0 +1,17 @@
+print("Please enter a number: ")
+number1 = int(input())
+
+print("Enter another number: ")
+number2 = int(input())
+
+print("Enter another number: ")
+number3 = int(input())
+
+add = number1 + number2 + number3
+print(str(number1) + " + " + str(number2) + " + " + str(number3) + " = " + str(add))
+
+subtract = number1 - number2 - number3
+print(str(number1) + " - " + str(number2) + " - " + str(number3) + " = " + str(subtract))
+
+multiply = number1 * number2 * number3
+print(str(number1) + " * " + str(number2) + " * " + str(number3) + " = " + str(multiply))
